6bba7b4bc602980c8b21d1f33f6750095db8be5f
vigneshdevelopr <vigneshtpr99@gmail.com>
2023-09-06 14:53:35 +0530

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import {Provider} from 'react-redux'
-import store from './Redux/Store/Store.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
 <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   </BrowserRouter>
-  </Provider>
 )
